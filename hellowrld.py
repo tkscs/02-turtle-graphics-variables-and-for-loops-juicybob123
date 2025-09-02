7f031a7fea2a9f0a089c8c0dc0237dae440b2cc1
juicybob123 <okruh2028@kehillahstudent.org>
2025-09-02 10:15:21 -0700

    aaa

---
+++ hellowrld.py
@@ -0,0 +1,17 @@
+from turtle import *
+right(90)
+integer = 1
+for loop  in range(7):
+    forward(50 * integer)
+    for loop in range(90):
+        y = y + 2
+        right(y)
+        forward(1)
+
+
+
+
+
+
+
+exitonclick()
