dd5eabf6391c632574f4bfc0464d8e07ef4bc9d4
juicybob123 <okruh2028@kehillahstudent.org>
2025-09-02 10:25:50 -0700

    for loop if cool

--- hellowrld.py
+++ hellowrld.py
@@ -1,17 +1,21 @@
 from turtle import *
 right(90)
 integer = 1
+y = 1
+
 for loop  in range(7):
-    forward(50 * integer)
-    for loop in range(90):
-        y = y + 2
-        right(y)
+    forward(100)
+    for loop in range(60):
+        left(3*y)
         forward(1)
+    if y == 1:
+        y = -1
+    else:
+        if y == -1:
+            y = 1
+   
 
-
-
-
-
-
-
+    
+    
+        
 exitonclick()
